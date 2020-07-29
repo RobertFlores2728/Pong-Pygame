126ffc05a5b818f6f5579daeaa1577d3040270d1
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-07-29 18:04:43 -0500

    Update Main.py
    added ball, and its wall and paddle collision behavior

--- Pong/Scripts/Main.py
+++ Pong/Scripts/Main.py
@@ -3,17 +3,25 @@ import sys, pygame
 pygame.init()
 
 
+
 def main():
-    screen = pygame.display.set_mode((1500, 1000)) # screen is a Surface object
+    screen = pygame.display.set_mode((1500, 1000))  # screen is a Surface object
     clock = pygame.time.Clock()
 
     blue = (100, 100, 255)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
 
     paddleRect = pygame.Rect(0, 0, 50, 200)
     paddleSpeed = 20
 
+    ballRect = pygame.Rect(int(screen.get_width() / 2), int(screen.get_height() / 2), 20, 20)
+    ballDirectionX = 1  # should be 1 or -1 only. ball either goes left or right
+    ballDirectionY = 1  # can be a number with greater fidelity. dictates curve of ball
+    ballSpeed = 10  # speed will increase with each hit
+
     while True:
-        clock.tick(30)
+        clock.tick(120)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,16 +32,29 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_p]:
             print("p was pressed!")
+        # Move paddle
         if keys[pygame.K_w]:
             paddleRect.y -= paddleSpeed
         if keys[pygame.K_s]:
             paddleRect.y += paddleSpeed
 
+        # paddle boundaries check
         if paddleRect.y < 0:
             paddleRect.y = 0
         elif paddleRect.y > screen.get_height() - paddleRect.height:
             paddleRect.y = screen.get_height() - paddleRect.height
-        print(screen.get_height())
+
+
+        # ball boundaries check, bounce around walls
+        if ballRect.x > screen.get_width() - ballRect.width:
+            ballDirectionX = -ballDirectionX
+        if ballRect.x < 0:
+            ballDirectionX = -ballDirectionX
+        if ballRect.y > screen.get_height() - ballRect.height:
+            ballDirectionY = -ballDirectionY
+        if ballRect.y < 0:
+            ballDirectionY = -ballDirectionY
+
 
 
 
@@ -41,11 +62,21 @@ def main():
         pygame.display.flip()
         screen.fill(blue)
 
-        # draw
-        pygame.draw.rect(screen, (0, 0, 0), paddleRect)
+        # draw paddle
+        pygame.draw.rect(screen, black, paddleRect)
 
 
+        # move ball
+        ballRect.x += ballDirectionX * ballSpeed
+        ballRect.y += ballDirectionY * ballSpeed
 
+        # draw ball
+        pygame.draw.rect(screen, white, ballRect)
+
+        # check collision
+        if paddleRect.colliderect(ballRect):
+            ballDirectionX = -ballDirectionX
+            print("Collision!")
 
 
 
