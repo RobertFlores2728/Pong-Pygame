89b250fe8a769cec2cf81215e5f48c4336b25e0c
RobertFlores2728 <68761226+RobertFlores2728@users.noreply.github.com>
2020-07-29 16:41:58 -0500

    Added Paddle movement
    setup code for paddle drawing and movement

--- Pong/Scripts/Main.py
+++ Pong/Scripts/Main.py
@@ -4,11 +4,17 @@ pygame.init()
 
 
 def main():
-    screen = pygame.display.set_mode((720, 480)) # screen is a Surface object
+    screen = pygame.display.set_mode((1500, 1000)) # screen is a Surface object
+    clock = pygame.time.Clock()
 
     blue = (100, 100, 255)
 
+    paddleRect = pygame.Rect(0, 0, 50, 200)
+    paddleSpeed = 20
+
     while True:
+        clock.tick(30)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -18,15 +24,26 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_p]:
             print("p was pressed!")
+        if keys[pygame.K_w]:
+            paddleRect.y -= paddleSpeed
+        if keys[pygame.K_s]:
+            paddleRect.y += paddleSpeed
 
-
+        if paddleRect.y < 0:
+            paddleRect.y = 0
+        elif paddleRect.y > screen.get_height() - paddleRect.height:
+            paddleRect.y = screen.get_height() - paddleRect.height
+        print(screen.get_height())
 
 
 
         # Update screen
-        screen.fill(blue)
-        pygame.draw.rect(screen, (0, 0, 0), [0, 0, 200, 200], 20)
         pygame.display.flip()
+        screen.fill(blue)
+
+        # draw
+        pygame.draw.rect(screen, (0, 0, 0), paddleRect)
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pong-Pygame
 Pong game made with Python 3.8.5 and Pygame 1.9.6
-IDE used: Pycharm Community Edition 2020.1.4
+IDE used: Pycharm Community Edition 2020
